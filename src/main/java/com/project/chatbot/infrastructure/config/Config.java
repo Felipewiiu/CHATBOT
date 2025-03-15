5bd1639433d77475a1a4ca0239d713ac65287e67
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2025-03-15 20:02:17 -0300

    fix: corrige as congigurações de beans

--- src/main/java/com/project/chatbot/infrastructure/config/Config.java
+++ src/main/java/com/project/chatbot/infrastructure/config/Config.java
@@ -2,25 +2,31 @@ package com.project.chatbot.infrastructure.config;
 
 import com.project.chatbot.adapters.gateways.UserRepositoryGateway;
 import com.project.chatbot.application.usecases.CreateUserUseCase;
+import com.project.chatbot.application.validators.UserValidator;
 import com.project.chatbot.infrastructure.gateway.UserRepositoryGatewayImpl;
 import com.project.chatbot.infrastructure.mapper.UserMapper;
 import com.project.chatbot.infrastructure.persistence.repository.UserRepository;
 import com.project.chatbot.presentation.controller.UserControllerImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class Config {
 
 
-
     @Bean
-    UserControllerImpl customUserController(CreateUserUseCase createUserUseCase) {
-        return new UserControllerImpl(createUserUseCase);
+    UserRepositoryGatewayImpl customUserRepositoryGatewayImpl(UserRepository userRepository, UserMapper userMapper) {
+        return new UserRepositoryGatewayImpl(userRepository, userMapper);
     }
 
     @Bean
-    UserRepositoryGatewayImpl customUserRepositoryGateway(UserRepository userRepository, UserMapper userMapper) {
-        return new UserRepositoryGatewayImpl(userRepository, userMapper);
+    CreateUserUseCase customCreateUserUseCase(
+            @Qualifier("customUserRepositoryGatewayImpl") UserRepositoryGateway userRepositoryGateway,
+            List<UserValidator> listUserValidation
+    ) {
+        return new CreateUserUseCase(userRepositoryGateway, listUserValidation);
     }
 }
