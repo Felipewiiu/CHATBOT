9156ac9ed71275c9e0209c94725672cf1ed80774
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2025-03-16 17:31:45 -0300

    chore: clean coder

--- src/main/java/com/project/chatbot/adapters/controllers/user/UserController.java
+++ src/main/java/com/project/chatbot/adapters/controllers/user/UserController.java
@@ -1,7 +1,6 @@
 package com.project.chatbot.adapters.controllers.user;
 
 import com.project.chatbot.adapters.controllers.dto.UserDto;
-import com.project.chatbot.domain.User;
 import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 
